070a1161947eab6577d5d760678f097d98bc77e9
Nethmal Wijesinghe <130041936+Nethmalwje@users.noreply.github.com>
2025-09-14 03:04:09 +0530

    static

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: "export",
+  output: "export",
   images: { unoptimized: true }, // optional: keeps export simple if you later add <Image/>
 };
 
